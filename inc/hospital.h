84c30e6dd8a3e3427f73bf3450e4cfe93c574de6
stepin104887 <69084828+stepin104887@users.noreply.github.com>
2020-09-18 23:04:48 +0530

    Create hospital.h

---
+++ inc/hospital.h
@@ -0,0 +1,15 @@
+#ifndef __HOSPITAL_H__
+#define __HOSPITAL_H__
+
+#include<stdio.h>
+#include<string.h>
+
+void read();
+void add();
+void view();
+void search();
+void edit();
+void del();
+void show();
+
+#endif
